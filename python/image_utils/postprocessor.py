3d89de9fa594c66db5664d5f66696d9f2afc3974
zeeshanRandhawa <zeeshan.randhawa@questlab.pk>
2024-08-27 16:00:49 +0500

    added new code

--- python/image_utils/postprocessor.py
+++ python/image_utils/postprocessor.py
@@ -17,20 +17,16 @@ class PostProcessor:
 
     def _white_to_transparency(self, img):
         x = np.asarray(img.convert('RGBA')).copy()
-
         x[:, :, 3] = (255 * (x[:, :, :3] != 255).any(axis=2)).astype(np.uint8)
-
         return Image.fromarray(x)
     
     def _tensor_to_pil_image(self, tensor):
         '''Convert PyTorch tensor to PIL Image'''
-
         transform = transforms.ToPILImage()
         return transform(tensor)
     
     def _pil_image_to_tensor(self, image):
         '''Convert PIL Image to PyTorch tensor'''
-
         return TF.to_tensor(image)
     
     def _crop_to_content(self, img):
@@ -41,73 +37,35 @@ class PostProcessor:
             img = img.crop(bbox)
         return img
 
-    def check_transparency(self, image):
-        '''Check transparency'''
-
-        if isinstance(image, str):
-            img = Image.open(image)
-        else:
-            image = self._tensor_to_pil_image(image)
-            img = image.copy()  # Make a copy to avoid modifying the original
-        
-        # Get the dimensions of the image
-        width, height = img.size
-        
-        # Loop through each pixel in the image
-        for y in range(height):
-            for x in range(width):
-                # Get the pixel value at (x, y)
-                r, g, b, a = img.getpixel((x, y))
-                
-                # Check if the alpha value is 0 (fully transparent)
-                is_transparent = (a == 0)
-                print(f"Pixel at ({x}, {y}) is {'transparent' if is_transparent else 'not transparent'}")    
-
-    def remove_white_background(self, image, transparency=0.7, threshold=240):
+    def remove_white_background(self, image, transparency=0.7):
         '''
-        Removes White background and makes it transparent, also makes the subject i.e the floorplan translucent
+        Removes white background and makes it transparent, also makes the subject translucent
         '''
         if isinstance(image, str):
             img = Image.open(image)
         else:
-            image = self._tensor_to_pil_image(image)
-            img = image.copy()  # Make a copy to avoid modifying the original
-
-
-        img = img.convert("RGBA")
-
+            img = image.convert("RGBA")  # Ensure image is in RGBA mode
+        
         img = self._white_to_transparency(img)
-        
-        img.putalpha(img.split()[-1].point(lambda p: p * transparency)) #for translucent
-
-        # img = self._crop_to_content(img)
-        # img = self._pil_image_to_tensor(img)
-
+        img.putalpha(img.split()[-1].point(lambda p: p * transparency))  # Make translucent
+        img = self._pil_image_to_tensor(img)
         return img
     
-    def remove_white_background_after(self, image, transparency=0.7, threshold=240):
+    def remove_white_background_after(self, image, transparency=0.7):
         '''
-        Removes White background and makes it transparent, also makes the subject i.e the floorplan translucent
+        Removes white background and makes it transparent, also makes the subject translucent
         '''
         if isinstance(image, str):
             img = Image.open(image)
         else:
-            image = self._tensor_to_pil_image(image)
-            img = image.copy()  # Make a copy to avoid modifying the original
-
-
-        img = img.convert("RGBA")
-
-        img = self._white_to_transparency(img)
+            img = self._tensor_to_pil_image(image)
         
-        img.putalpha(img.split()[-1].point(lambda p: p * transparency)) #for translucent
-
+        img = img.convert("RGBA")
+        img = self._white_to_transparency(img)
+        img.putalpha(img.split()[-1].point(lambda p: p * transparency))  # Make translucent
         img = self._crop_to_content(img)
         img = self._pil_image_to_tensor(img)
-
         return img
-
-
 #Testing - commented    
 # postprocessor = PostProcessor()
 # input_image_path = r"D:\Floor generator\houseganpp\dump\fp_final_2.png"
